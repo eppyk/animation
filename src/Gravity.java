036ab55e63c86631a897225a343d7a16148add20
Kinga <k.kieczkowska@gmail.com>
2015-12-10 10:12:37 +0100

    Almost works but not quite yet

--- src/Gravity.java
+++ src/Gravity.java
@@ -1,6 +1,5 @@
 import java.awt.image.BufferedImage;
 import java.awt.*;
-import java.awt.Graphics2D;
 import java.io.FileOutputStream;
 import javax.imageio.ImageIO;
 import net.jmge.gif.Gif89Encoder;
@@ -33,28 +32,39 @@ public class Gravity extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 
-	static void drawFrameDown(Graphics2D g, int height, int h){
+	static void drawFrameDown(Graphics2D g, int height){
+		g.setColor(Color.red);
+	    g.fillRect(0, 0, 300, 300);	
+	    g.setColor(Color.blue);
+
+		for (int h=0; h<height; h++){
+		    g.fillOval(height+h,0,20,20);
+		}
+	}
+	
+	static void drawFrameUp(Graphics2D g, int height){
 		g.setColor(Color.red);
 	    g.fillRect(0, 0, 300, 300);
 	    g.setColor(Color.blue);
-	    g.fillOval(0,height-h,20,20);
+	 
+		for (int h=0; h<height; h++){
+		    g.fillOval(0,height-h,20,20);
 		}
+	}
 	
-	static void drawFrameUp(Graphics2D g, int height, int h){
-		g.setColor(Color.red);
-	    g.fillRect(0, 0, 300, 300);
-	    g.setColor(Color.blue);
-	    g.fillOval(0,height-h,20,20);
-		}
-	
+    static void drop(Graphics2D g, int x, int y) {
+    	g.setColor(Color.blue);
+    	g.fillOval(x, y, 50, 50);
+    }
+    
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	
+
+		if (request.getParameter("mass")==null){
+			PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
-		
-		PrintWriter out = response.getWriter();
-		
-		//taking the input from the user - initial height at which the ball is
+				//taking the input from the user - initial height at which the ball is
 		//held, its mass, energy loss coefficient, number of bounces
 		out.println("<h2>An animation of a bouncing ball</h2>");
 		out.println("<form>");
@@ -72,7 +82,10 @@ public class Gravity extends HttpServlet {
 		out.println("<input type='submit'/>");
 		out.println("</form>");
 
-		try {
+		} else {
+			
+			try {
+		
 		//parsing all the parameters
 		int initialHeight = Integer.parseInt(request.getParameter("height"));
 		int mass = Integer.parseInt(request.getParameter("mass"));
@@ -82,34 +95,48 @@ public class Gravity extends HttpServlet {
 		//physics magic - calculating 
 		int g = 10;
 
-		int height = initialHeight;
+		float height = initialHeight;
+		float ep = initialHeight*mass*g;
+		int width = 1000;
+		int heightImg = 1500;
+		int x = width / 2 - 25;;
+		int y = 0;
+		int vel = 5;
 		
 		Gif89Encoder genc = new Gif89Encoder();
-		for (int i=0; i<bounceNo; i++){
-			int ep = initialHeight*mass*g;
-			out.println(ep);
-			for (int h=0; h<height; h++){
-				out.println(ep);
-				BufferedImage image = new BufferedImage(150,100,BufferedImage.TYPE_INT_ARGB);
-				Graphics2D g1 = image.createGraphics();
-			    drawFrameDown(g1, height, h);
-		        genc.addFrame(image);
-		        g1.dispose();
-		        ep = height - (k/100)*ep;
-		        height = ep/(mass*g);
-				BufferedImage image2 = new BufferedImage(150,100,BufferedImage.TYPE_INT_ARGB);
-				Graphics2D g2 = image.createGraphics();
-		        drawFrameUp(g2, height, h);
-			}	}
+		for (int i = 1; i < 500; i++) {
+			BufferedImage image = new BufferedImage(width, heightImg, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g1 = image.createGraphics();
+			g1.setColor(Color.decode("#AAAAAA"));
+			g1.fillRect(0, 0, width, heightImg);
+
+			if (y > height - 50 && vel < 5 && vel >= 0){
+				g1.setColor(Color.blue);
+				g1.fillOval(x, heightImg - 45, 50, 50);
+			} else {
+				drop(g1, x, y);
+				if (y >= heightImg - 50 && vel > 20)
+					vel = -vel + 10;
+				else if (y >= heightImg - 50 && vel > 0)
+					vel = -vel + 5;
+				y += vel;
+				if (vel < 0)
+					vel++;
+				if (i % 5 == 0 && vel < 40)
+					vel += 10;
+			}
 			
 		genc.setUniformDelay(10);
         genc.setLoopCount(0);
         genc.encode(response.getOutputStream());
 		
-	} catch(Exception e){
+	} 
+			}catch(Exception e){
 		System.err.println(e);
 	}
 	
+	}	
+			
 	}
 	
 	/**
